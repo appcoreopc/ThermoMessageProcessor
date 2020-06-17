d89129a4ceb2679a04e9b2ed271152847e8ccef9
jeremywoowk <devaksaz@outlook.com>
2020-06-17 15:49:45 +0800

    Added data processor

--- PersonelTemperatureHandlingFunction/ThemoDataMessageProcessor/PersonelThemoDataHandler/IMesssageProcessor.cs
+++ PersonelTemperatureHandlingFunction/ThemoDataMessageProcessor/PersonelThemoDataHandler/IMesssageProcessor.cs
@@ -1,9 +1,9 @@
-﻿using Newtonsoft.Json;
+﻿using System.Threading.Tasks;
 
 namespace ThemoDataMessageProcessor.PersonelThemoDataHandler
 {
     public interface IMesssageThermoProcessor
     {
-        T ProcessMessage<T>(string message);
+        Task ProcessMessage(string message);
     }
 }
--- PersonelTemperatureHandlingFunction/ThemoDataMessageProcessor/PersonelThemoDataHandler/PersonelThermoMessageProcessor.cs
+++ PersonelTemperatureHandlingFunction/ThemoDataMessageProcessor/PersonelThemoDataHandler/PersonelThermoMessageProcessor.cs
@@ -1,20 +1,30 @@
 ﻿using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
+using System.Threading.Tasks;
+using ThemoDataMessageProcessor.DataProcessor;
+using ThemoDataModel;
 
 namespace ThemoDataMessageProcessor.PersonelThemoDataHandler
 {
     public class PersonelThermoMessageProcessor : IMesssageThermoProcessor
     {
         private readonly ILogger _logger;
-        public PersonelThermoMessageProcessor(ILogger logger)
+        private readonly IMessageController _messageController;
+
+        public PersonelThermoMessageProcessor(ILogger logger
+            )
         {
             _logger = logger;
+           // _messageController = messageController;
         }
-        public T ProcessMessage<T>(string message)
+        public Task ProcessMessage(string message)
         {
-            this._logger.LogInformation($"Processing message at {DateTime.Now} : {message}");
-            return JsonConvert.DeserializeObject<T>(message);
+            this._logger.LogInformation($"Processing message using PersonelThermoMessageProcessor  {DateTime.Now} : {message}");
+
+            var targetData = JsonConvert.DeserializeObject<PersonelThermoDataModel>(message);
+            this._messageController.ProcessDataAsync(targetData);
+            return Task.CompletedTask;
         }
     }
 }
